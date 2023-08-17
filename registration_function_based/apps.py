f22d9e9c3ac0f4b26e71f9d49532f8df79ae97b1
Mohit Saxena <mksaxena27@gmail.com>
2023-08-17 12:31:46 +0530

    delete

--- registration_function_based/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegistrationFunctionBasedConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'registration_function_based'
